1b330bbb075a64bf4d986ee289367ff719444675
DhanashriJoshi <dhanashrijoshi91@gmail.com>
2019-04-03 11:27:51 +0530

    root added to the application

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     end
   end
   resources :patients
+  root to: "home#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
