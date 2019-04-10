326492b9102c2438ca232843577f0a1c04a00325
DhanashriJoshi <dhanashrijoshi91@gmail.com>
2019-04-10 15:52:15 +0530

    api stub demo

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,9 @@
 class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
+
+  def self.get_doctors_list_from_api_call
+    response = RestClient.get 'https://demo6333249.mockable.io/get_list_of_doctors_name'
+    # JSON.parse response.body
+  end
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.get_doctors_list_from_api_call' do
+    context "with valid Request" do
+      let(:uri) { 'https://demo6333249.mockable.io/get_list_of_doctors_name' }
+      let(:body) { "{\n \"doctors\": {\n     \"names\": [\n         \"Dr. A\",\n         \"Dr. B\"\n         ]\n }\n}\n" }
+      it "gets a JSON of Name of Doctors List" do
+        stub_get = WebMock.stub_request(:get, uri).to_return(status: 200, body: body)
+        stub_response = stub_get.response
+        resp = Doctor.get_doctors_list_from_api_call
+        expect(stub_response.status).to include(resp.code)
+        expect(resp.body).to eq(stub_response.body)
+      end
+    end
+  end
 end
